fc9c9e70900d38acfaf23d5aa613f3f52b8576cd
experimental36 <alakazam36@gmail.com>
2015-06-07 00:22:30 +0200

    plot2

--- plot1.R
+++ plot1.R
@@ -6,3 +6,5 @@ power_sub <- power_table[66637:69516,]
 new_global_active_power <- as.numeric(as.vector(power_sub$Global_active_power))
 hist(new_global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
 
+dev.copy(png, file = "plot1.png")
+dev.off()
