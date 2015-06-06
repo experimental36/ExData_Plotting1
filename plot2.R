e5d8d2d8d9cf1e24dc6ed700fdfcf57aab47f955
experimental36 <alakazam36@gmail.com>
2015-06-07 00:22:12 +0200

    plot2

---
+++ plot2.R
@@ -0,0 +1,16 @@
+library(data.table)
+power_data <- read.table("power_data.txt", nrows = 69600, sep = ";", head = TRUE)
+power_table <- data.frame(power_data)
+power_sub <- power_table[66637:69516,]
+
+new_global_active_power <- as.numeric(as.vector(power_sub$Global_active_power))
+Sys.setlocale("LC_TIME", "eng")
+
+merged <- cbind(paste(power_sub$Date,power_sub$Time))
+converted <- as.POSIXct(merged, format="%d/%m/%Y %H:%M:%S")
+
+par(mar = c(3,4,2,1))
+plot(converted, new_global_active_power, type="l", ylab = "Global Active Power (kilowatts)")
+
+dev.copy(png, file = "plot2.png")
+dev.off()
