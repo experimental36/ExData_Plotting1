0c97ca497ee03513f5328125cec53efe4ea65957
experimental36 <alakazam36@gmail.com>
2015-06-07 12:58:13 +0200

    popravek plot4

--- plot4.R
+++ plot4.R
@@ -21,19 +21,22 @@ png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2))
 
 par(mar = c(6,4,3,2))
-plot(converted, new_global_active_power, type="l", xlab = "", ylab = "Global Active Power")
+plot(converted, new_global_active_power, type="l", 
+     xlab = "", ylab = "Global Active Power")
 
 par(mar = c(6,4,3,2))
 plot(converted, voltage, type="l", xlab = "datetime", ylab = "Voltage")
 
 par(mar = c(6,4,3,2))
-plot(converted, sub1, type="l", ylab = "Energy sub metering")
-lines(converted, sub2, col="red")
-lines(converted, sub3, col="blue")
-legend("topright", lty =1 , bty = "n", col = c("black", "red", "blue"),
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+plot(converted, sub1, type="l", 
+  ylab = "Energy sub metering", xlab = "")
+  lines(converted, sub2, col="red")
+  lines(converted, sub3, col="blue")
+  legend("topright", lty =1 , bty = "n", col = c("black", "red", "blue"),
+  legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 par(mar = c(6,4,3,2))
-plot(converted, new_global_reactive_power, type="l", xlab = "datetime", ylab = "Global_reactive_power")
+plot(converted, new_global_reactive_power, type="l", 
+     xlab = "datetime", ylab = "Global_reactive_power")
 
 dev.off()
